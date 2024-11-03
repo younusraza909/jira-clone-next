25e6bdf75c7b995343311f0e87a6b7d33a2dfba3
younusraza909 <younusraza909@gmail.com>
2024-11-03 15:11:27 +0500

    Added more components from shadcn and updated few per requirnment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,14 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-      <Button variant="destructive">Hello World</Button>
-      <p className="text-red-500 font-semibold">Antonio</p>
+    <div className="">
+      <Button>Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="muted">Link</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="teritary">Teritary</Button>
     </div>
   );
 }
